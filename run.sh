c82f56b59e709fbed581ad249765409b9b3e41d9
Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>
2024-02-27 14:44:06 +0100

    feat: change ros domain id
    Signed-off-by: Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 rocker --network host --privileged --nvidia --x11 --user --name honey_badger \
     --env "USER" \
     --env "RMW_IMPLEMENTATION=rmw_fastrtps_cpp" \
-    --env "ROS_DOMAIN_ID=0" \
+    --env "ROS_DOMAIN_ID=70" \
     --volume "${PWD}:${HOME}/${PWD##*/}" \
     --volume /dev/:/dev/ \
     -- macnack/honey_badger_project:humble
