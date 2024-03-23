b003242f786c4b9dae07ecf84699a9ece9f07d0f
macnack <maciej.p.krupka@student.put.poznan.pl>
2024-03-23 13:05:39 +0100

    feat: add symlink to global.json
    Signed-off-by: macnack <maciej.p.krupka@student.put.poznan.pl>

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.ros/mab/config > /dev/null 2>&1
 set -e
 colcon build --symlink-install --executor parallel --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DCMAKE_BUILD_TYPE=Release -Wall -Wextra -Wpedantic
-if ([ ! -f ~/.ros/mab/config/global.json ] && [ $# -eq 0 ]) then
+if ([ ! -f ~/.ros/mab/config/global.json ]) then
     echo "Copying local config from repo to ~/.ros/mab/config/global.json"
-    cp install/hb40_commons/share/hb40_commons/config/global.json ~/.ros/mab/config/global.json
+    ln -s {$HOME}/honey_badger_ws/src/hb40/src/hb40_commons/config ~/.ros/mab/config/global.json
 fi
 
 echo "Syncing workspace"
